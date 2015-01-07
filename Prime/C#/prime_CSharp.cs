d4c9e65317fac8f991179826961e430879396654
Noel Bao <noeleon930@gmail.com>
2015-01-08 04:28:33 +0800

    [Add] Prime Ver. C#
    and include its portOut.txt

---
+++ Prime/C#/prime_CSharp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace prime_CSharp
+{
+    class prime_CSharp
+    {
+        const int targetNumber = 10000;
+
+        static void Main(string[] args)
+        {
+            /*Timer start!*/
+            var start = Stopwatch.StartNew();
+
+            /*Initialize the bool*/
+            bool isPrime;
+        
+            /*Smallest prime number*/
+            Console.WriteLine("2");
+
+            /*Try from 3 to targetNumber.
+            And the step is 2.
+            Except 2, any other prime numbers are all odd.*/
+            for(int i = 3; i < targetNumber; i+=2)
+            {
+                /*Reset to true in every new loop*/
+                isPrime = true;
+            
+                /*At most check to its sqrt.*/
+                for(int j = 3; j <= Math.Sqrt(i); j++)
+                {
+                    if(i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            
+                if(isPrime) Console.WriteLine(i);
+            }
+ 
+            /*Timer stop!*/
+            start.Stop();
+
+            /*Count the elapsed time*/
+            var elapsedTime = start.ElapsedMilliseconds;
+ 
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("targetNumber is " + targetNumber);
+            Console.WriteLine("elapsedTime is " + elapsedTime + " ms");
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
